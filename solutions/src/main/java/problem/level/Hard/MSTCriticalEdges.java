c7d096a4cb97bcd9955dc55bdfd4f9de86ae3f4c
aalsanie <aalsanie@expedia.com>
2021-08-23 13:50:59 +0300

    mst

---
+++ solutions/src/main/java/problem/level/Hard/MSTCriticalEdges.java
@@ -0,0 +1,7 @@
+package problem.level.hard;
+
+public class MSTCriticalEdges {
+
+    //find both critical edges and non-critical edges that builds up all possible MSTs
+    // a critical edge exists in every possible MST
+}
